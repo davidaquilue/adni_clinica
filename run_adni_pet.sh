7afc153502e8e1671048f5390284276834bb6a86
david <david@petprepro.myguest.virtualbox.org>
2025-03-25 16:39:43 +0100

    First attempt PET processing

--- run_adni_pet.sh
+++ run_adni_pet.sh
@@ -1,9 +1,15 @@
 
 DATA_DIR="/adni_data/ADNI"
 BIDS_DIR="/adni_data/ADNI_BIDS"
-CLIN_DIR="/adni_data/ADNI_CLINICAL/"
+CAPS_DIR="/adni_data/ADNI_CAPS/"
+ACQ_LABEL="18FAV45"
+TSV_FILE="/home/david/Desktop/code/adni_clinica/subset.tsv"
+PVC_PSF_TSV="/home/david/Desktop/code/adni_clinica/subset_pvc_psf.tsv"
+WD_DIR="/adni_data/clinica_wd/"  # Working directory for clinica
+NPROCS=4  # Number of cores to be used.
 
-clinica convert adni-to-bids -m PET_AMYLOID $DATA_DIR $CLIN_DIR $BIDS_DIR
+# Reference will be cerebellumPons at first
+clinica run pet-surface $BIDS_DIR $CAPS_DIR $ACQ_LABEL cerebellumPons $PVC_PSF_TSV -tsv $TSV_FILE -np $NPROCS -wd $WD_DIR
 
 # I am missing the files to continue this approach. We will see if it's worth it
 # to continue using clinica at some point as a backbone, could collaborate with the project.
